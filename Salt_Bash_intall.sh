71f9e180f0302674367a6623051de23fa473e70c
Willem_Slager <w.slager123@gmail.com>
2018-06-27 17:44:32 +0200

    Bash

--- Salt_Bash_intall.sh
+++ Salt_Bash_intall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-read -rp 'What do you wish to install? (Master/Minion)' installvar
-read -rp 'What is the Master ip' mstr_ipvar
+read -rp 'What do you wish to install? (Master/Minion):' installvar
+read -rp 'What is the Master ip:' mstr_ipvar
 
 if [ "$installvar" = "Master" ]; then
         curl -L https://bootstrap.saltstack.com -o install_salt.sh
-        sudo sh install_salt.sh -MAU "$mstr_ipvar"
+        sudo sh install_salt.sh -MAU $mstr_ipvar
         while true; do
                 salt-key --accept-all
-                sleep NUMBER[30]
+                sleep 30
         done
 
 fi
