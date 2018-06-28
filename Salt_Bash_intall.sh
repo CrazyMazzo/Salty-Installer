bf4880e58fabceefe121d36dbb2a0814e296aca0
Willem_Slager <w.slager123@gmail.com>
2018-06-28 23:00:20 +0200

    while_working

--- Salt_Bash_intall.sh
+++ Salt_Bash_intall.sh
@@ -1,42 +1,38 @@
 #!/bin/bash
 
 mstr_ipvar=$1
-while_working=true
 ready="n"
 
 
 
-while [ $while_working ]; do
-    read -rp 'What do you wish to install? (Master/Minion): ' installvar
-    if [ "$installvar" = "Master" ]; then
-            curl -L https://bootstrap.saltstack.com -o install_salt.sh
-            sudo sh install_salt.sh -M -U -A "$mstr_ipvar"
-            echo "Install Minion"
-            sudo sh install_salt.sh -U -A "$mstr_ipvar"
 
-            sudo mkdir /srv/salt
-            sudo rm /srv/salt/*
-            sudo mv ~/Salty-Installer/*.sls /srv/salt/
-            echo "hi"
-            while [ $ready = "n" ]; do
-            echo "bye"
-                echo $n/2 minutes left
-                sudo salt-key --accept-all -y
-                sleep 30
+read -rp 'What do you wish to install? (Master/Minion): ' installvar
+if [ "$installvar" = "Master" ]; then
+        curl -L https://bootstrap.saltstack.com -o install_salt.sh
+        sudo sh install_salt.sh -M -U -A "$mstr_ipvar"
+        echo "Install Minion"
+        sudo sh install_salt.sh -U -A "$mstr_ipvar"
 
-                sudo salt-key
-                read -rp 'Are the keys accepted? (y/n): ' ready
-            done
-            while_working=false
+        sudo mkdir /srv/salt
+        sudo rm /srv/salt/*
+        sudo mv ~/Salty-Installer/*.sls /srv/salt/
+        echo "hi"
+        while [ $ready = "n" ]; do
+        echo "bye"
+            sudo salt-key --accept-all -y
+            sudo salt-key
+            read -rp 'Are the keys accepted? (y/n): ' ready
+            echo "Waiting 10 seconds"
+            sleep 10
+        done
 
-    elif [ "$installvar" = "Minion" ]; then
-            curl -L https://bootstrap.saltstack.com -o install_salt.sh
-            sudo sh install_salt.sh -U -A "$mstr_ipvar"
-            sudo mkdir /srv/salt
-            sudo rm /srv/salt/*
-            sudo mv ~/Salty-Installer/*.sls /srv/salt/
-            while_working=false
-    fi
-done
+elif [ "$installvar" = "Minion" ]; then
+        curl -L https://bootstrap.saltstack.com -o install_salt.sh
+        sudo sh install_salt.sh -U -A "$mstr_ipvar"
+        sudo mkdir /srv/salt
+        sudo rm /srv/salt/*
+        sudo mv ~/Salty-Installer/*.sls /srv/salt/
+fi
+
 
 echo "Salt Installer done!!"
