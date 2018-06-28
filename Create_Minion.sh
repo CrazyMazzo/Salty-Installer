bb4caf437c58b59cadd6b65105366489a9282948
Willem_Slager <w.slager123@gmail.com>
2018-06-29 00:36:24 +0200

    sls file

--- Create_Minion.sh
+++ Create_Minion.sh
@@ -2,11 +2,11 @@
 
 read -rp 'What is the Master_IP: ' mstr_ip
 
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
 sudo chmod 755 ~/Salty-Installer/Salt_Bash_intall.sh
 sudo ~/Salty-Installer/Salt_Bash_intall.sh $mstr_ip
 
 sudo chmod 755 ~/Salty-Installer/Minion_Nagios_Bash.sh
-
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
