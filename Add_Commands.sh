87b9a60733acb7ea3ac73b68791dcd3d68293019
Willem_Slager <w.slager123@gmail.com>
2018-06-28 22:24:30 +0200

    Master ???

--- Add_Commands.sh
+++ Add_Commands.sh
@@ -4,7 +4,7 @@ minion_name=$1
 minion_ip=$2
 
 
-sudo ./Adding_Nagios_server.sh $minion_name $minion_ip
+sudo ~/Salty-Installer/Adding_Nagios_server.sh $minion_name $minion_ip
 
 sudo salt "$minion_name" state.apply -t 60 Minion_Nagios_Bash
 sudo salt "$minion_name" cmd.run 'sudo systemctl restart nrpe.service'
--- Create_Master.sh
+++ Create_Master.sh
@@ -3,10 +3,10 @@
 mstr_ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
 sudo chmod 755 ~/Salty-Installer/Salt_Bash_intall.sh
-sudo ./Salt_Bash_intall $mstr_ip
+sudo ~/Salty-Installer/Salt_Bash_intall $mstr_ip
 
 sudo chmod 755 ~/Salty-Installer/Nagios_install_bash.sh
-sudo ./Nagios_install_bash.sh
+sudo ~/Salty-Installer/Nagios_install_bash.sh
 
 read -rp 'Is the Minion ready? (y/n): ' ready
 
@@ -25,7 +25,7 @@ sudo chmod 755 ~/Salty-Installer/Add_Commands.sh
 sudo salt "58-UBU1604-SaltMaster" state.apply -t 60 Master_Syslog_Installer
 sudo salt "58-UBU1604-SaltMaster" cmd.run 'sudo systemctl restart syslog-ng'
 
-sudo ./Add_Commands.sh $minion_name $minion_ip
+sudo ~/Salty-Installer/Add_Commands.sh $minion_name $minion_ip
 
 
 
