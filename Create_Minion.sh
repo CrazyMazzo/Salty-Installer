242ae910176c41a58268db6e764caf9f5759cc38
Willem_Slager <w.slager123@gmail.com>
2018-06-28 23:27:16 +0200

    Create Minion

--- Create_Minion.sh
+++ Create_Minion.sh
@@ -3,7 +3,7 @@
 read -rp 'What is the Master_IP: ' mstr_ip
 
 sudo chmod 755 ~/Salty-Installer/Salt_Bash_intall.sh
-sudo ~/Salty-Installer/Salt_Bash_intall.shy $mstr_ip
+sudo ~/Salty-Installer/Salt_Bash_intall.sh $mstr_ip
 
 sudo chmod 755 ~/Salty-Installer/Minion_Nagios_Bash.sh
 
