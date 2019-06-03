96c1eb7a258c2c4b43f99c0e8174d8985eefe01b
Willem Slager <w.slager123@gmail.com>
2019-06-03 13:01:30 +0200

    Update Create_Minion.sh

--- Create_Minion.sh
+++ Create_Minion.sh
@@ -6,7 +6,13 @@ sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
+sudo mkdir /srv/scripts
+sudo rm /srv/scripts/*
+
 sudo chmod 755 ~/Salty-Installer/Salt_Bash_intall.sh
 sudo ~/Salty-Installer/Salt_Bash_intall.sh $mstr_ip
 
 sudo chmod 755 ~/Salty-Installer/Minion_Nagios_Bash.sh
+
+sudo cd ~/Salty-Installer
+sudo cp Minion_Nagios_Bash.sh /srv/scripts
