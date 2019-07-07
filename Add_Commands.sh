f42a09b3f3f22ab6574483b977040a613c3a998d
Willem Slager <w.slager123@gmail.com>
2019-07-07 21:43:35 +0200

    Update Add_Commands.sh

--- Add_Commands.sh
+++ Add_Commands.sh
@@ -6,6 +6,7 @@ minion_ip=$2
 
 sudo ~/Salty-Installer/Adding_Nagios_server.sh $minion_name $minion_ip
 
+sudo salt "$minion_name" cmd.run 'sudo /srv/scripts/Minion_Nagios_Bash.sh'
 sudo salt "$minion_name" state.apply -t 60 Minion_Nagios_Bash
 sudo salt "$minion_name" cmd.run 'sudo systemctl restart nrpe.service'
 
